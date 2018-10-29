0a9fab2d78f5ed7c477d9ee291e5245e0b892f15
marcosjrs <marcosjrs80@gmail.com>
2018-10-29 19:35:38 +0100

    Creacion de rutas y add al modulo de app

---
+++ src/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './app/components/home/home.component';
+import { SearchComponent } from './app/components/search/search.component';
+
+export const ROUTES:Routes = [
+    {path:"home", component:HomeComponent},
+    {path:"search", component:SearchComponent},
+    {path:"**", redirectTo:"/home"}
+];
